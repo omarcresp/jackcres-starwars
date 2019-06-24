087f978e1ff01859b038bea1647612cc7f926497
Git Server Admin <admin@git-server>
2019-06-24 16:26:01 +0000

    Add initial repository

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'May the Force be with you';
+}
